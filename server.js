c6cf208e6b22a24829e8d1412ea239428d752698
Walker Logan <walker.logan44@gmail.com>
2023-05-04 22:22:07 -0500

    debugging save feature on server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const path = require("path");
 const uniqid = require("uniqid");
 const app = express();
+const DB_PATH = path.join(__dirname, "Develop", "db", "db.json");
 const PORT = process.env.PORT || 3001;
 
 // middleware
@@ -17,42 +18,27 @@ app.get("/notes", (req, res) =>
 
 // reads the db.json file
 app.get("/api/notes", function (req, res) {
-  fs.readFile("./db/db.json", (err, data) => res.send(data));
+  fs.readFile(DB_PATH, (err, data) => res.send(data));
 });
 
-// function to read notes from the json file
-const readNotes = (callback) => {
-  fs.readFile("./db/db.json", "utf8", (err, data) => {
-    if (err) throw err;
-    const notes = JSON.parse(data);
-    callback(notes);
-  });
-};
-
-// function to write notes to the json file
-const writeNotes = (notes, callback) => {
-  fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
-    if (err) throw err;
-    callback();
-  });
-};
-
-// route to create a new note and save it to the json file
 app.post("/api/notes", (req, res) => {
-  readNotes((notes) => {
+  fs.readFile(DB_PATH, "utf8", (err, data) => {
+    let notes = JSON.parse(data);
     const newNote = {
       title: req.body.title,
       text: req.body.text,
       id: uniqid(),
     };
     notes.push(newNote);
-    writeNotes(notes, () => {
+    fs.writeFile(DB_PATH, JSON.stringify(notes), (err) => {
+      if (err) {
+        console.log("error");
+      }
       res.json(newNote);
     });
   });
 });
 
-// route to serve the index.html file for all other routes
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "public/index.html"))
 );
@@ -60,11 +46,11 @@ app.get("*", (req, res) =>
 // delete data
 app.delete("/api/notes/:id", (req, res) => {
   const noteId = req.params.id;
-  fs.readFile("./db/db.json", "utf8", (err, data) => {
+  fs.readFile(DB_PATH, "utf8", (err, data) => {
     if (err) throw err;
     const notes = JSON.parse(data);
     const updatedNotes = notes.filter((note) => note.id != noteId);
-    fs.writeFile("./db/db.json", JSON.stringify(updatedNotes), (err) => {
+    fs.writeFile(DB_PATH, JSON.stringify(updatedNotes), (err) => {
       if (err) throw err;
       res.json(updatedNotes);
     });
